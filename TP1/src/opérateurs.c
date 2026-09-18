58dea8aabcc4e9b84ce965640994462a9c63b2e4
nolan.95160 <nolanlefaoupro@gmail.com>
2026-09-18 09:15:40 +0000

    TP-1.5

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int a = 16;
+    int b = 3;
 
+    printf("Addition (a + b) : %d\n", a + b);
+
+    printf("Soustraction (a - b) : %d\n", a - b);
+
+    printf("Multiplication (a * b) : %d\n", a * b);
+
+    printf("Division (a / b) : %d\n", a / b);
+
+    printf("Modulo (a %% b) : %d\n", a % b);
+
+    printf("a est-il égal à b ? (a == b) : %d\n", a == b);
+
+    printf("a est-il supérieur à b ? (a > b) : %d\n", a > b);
+
+    return 0;
+}
