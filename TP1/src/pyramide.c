5a8c56f0a4f4b45466d3118eab0056cfdc3feb07
nolan.95160 <nolanlefaoupro@gmail.com>
2026-09-18 09:22:45 +0000

    TP-1.10

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    int n = 5;
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+
+    return 0;
+}
