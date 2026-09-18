96c0aa47e91ed5ca907819e45a26a57d6b8b66fa
nolan.95160 <nolanlefaoupro@gmail.com>
2026-09-18 09:07:10 +0000

    TP-1.3

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,1 +1,25 @@
+int main() {
+    
+    printf("char               : %zu octet(s)\n", sizeof(char));
+    printf("signed char        : %zu octet(s)\n", sizeof(signed char));
+    printf("unsigned char      : %zu octet(s)\n", sizeof(unsigned char));
 
+    printf("short              : %zu octet(s)\n", sizeof(short));
+    printf("unsigned short     : %zu octet(s)\n", sizeof(unsigned short));
+
+    printf("int                : %zu octet(s)\n", sizeof(int));
+    printf("unsigned int       : %zu octet(s)\n", sizeof(unsigned int));
+
+    printf("long int           : %zu octet(s)\n", sizeof(long int));
+    printf("unsigned long int  : %zu octet(s)\n", sizeof(unsigned long int));
+
+    printf("long long int      : %zu octet(s)\n", sizeof(long long int));
+    printf("unsigned long long : %zu octet(s)\n", sizeof(unsigned long long int));
+
+    // Types flottants : pas de signed/unsigned
+    printf("float              : %zu octet(s)\n", sizeof(float));
+    printf("double             : %zu octet(s)\n", sizeof(double));
+    printf("long double        : %zu octet(s)\n", sizeof(long double));
+
+    return 0;
+}
