4c9f7fee912bd355005421800c215f0462f79062
nolan.95160 <nolanlefaoupro@gmail.com>
2026-09-18 09:20:12 +0000

    TP-1.8

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
 
+int main() {
+    int num1 = 15;
+    int num2 = 4;
+    char op = '+'; 
+
+    switch (op) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            } else {
+                printf("Erreur : Division par zero\n");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            } else {
+                printf("Erreur : Modulo par zero\n");
+            }
+            break;
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+        default:
+            printf("Erreur : Operateur non valide\n");
+            break;
+    }
+
+    return 0;
+}
